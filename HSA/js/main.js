b306e100fca366f7dad3b45b4f3457b806483927
nhugh406 <nhughes@basusa.com>
2020-01-03 17:14:07 -0500

    enhancing calculation logic for remaing

--- HSA/js/main.js
+++ HSA/js/main.js
@@ -7,17 +7,22 @@ var HSAInput = new Vue ({
         hsaMax: 7100,
         perPayCount: 26,
         chosenHSAPlan: 'HSA Plan 1',
+        hsaAnnualDone: 1000,
+        hsaAnnualRem: null
         
 
     },
     watch: {
         hsaAnnualVal: function() {
-            if (this.hsaAnnualVal <= this.hsaMax) {
-                this.hsaBWK = (Number(this.hsaAnnualVal) / this.perPayCount).toFixed(2);
+            if ((Number(this.hsaAnnualVal) + this.hsaAnnualDone) <= this.hsaMax) {
+                this.hsaBWK = ((Number(this.hsaAnnualVal) - Number(this.hsaAnnualDone)) / this.perPayCount).toFixed(2);
             } else {
-                this.hsaBWK = (Number(this.hsaMax) / this.perPayCount).toFixed(2);
+                this.hsaBWK = ((Number(this.hsaAnnualVal) - Number(this.hsaAnnualDone) )/ this.perPayCount).toFixed(2);
+            }
+            if (Number(this.hsaAnnualVal) > this.hsaMax ) {
                 this.hsaAnnualVal = this.hsaMax;
             }
+            this.hsaAnnualRem = this.hsaAnnualVal - this.hsaAnnualDone;
         },
        
         hsaOpt: function() {
