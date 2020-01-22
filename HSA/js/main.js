e99cdde72133000cff20b60c48196196f3277d1f
nhugh406 <nhughes@basusa.com>
2020-01-22 12:07:47 -0500

    adding switch scenario

--- HSA/js/main.js
+++ HSA/js/main.js
@@ -1,4 +1,4 @@
-var HSAInput = new Vue ({
+var HSAInput = new Vue({
     el: '#hsaInputArea',
     data: {
         hsaAnnualVal: null,
@@ -7,26 +7,53 @@ var HSAInput = new Vue ({
         hsaMax: 7100,
         perPayCount: 26,
         chosenHSAPlan: 'HSA Plan 1',
-        hsaAnnualDone: 1000,
+        hsaAnnualDone: null,
         hsaAnnualRem: null
-        
+
 
     },
     watch: {
-        hsaAnnualVal: function() {
+        hsaAnnualVal: function () {
             if ((Number(this.hsaAnnualVal) + this.hsaAnnualDone) <= this.hsaMax) {
                 this.hsaBWK = ((Number(this.hsaAnnualVal) - Number(this.hsaAnnualDone)) / this.perPayCount).toFixed(2);
             } else {
-                this.hsaBWK = ((Number(this.hsaAnnualVal) - Number(this.hsaAnnualDone) )/ this.perPayCount).toFixed(2);
+                this.hsaBWK = ((Number(this.hsaAnnualVal) - Number(this.hsaAnnualDone)) / this.perPayCount).toFixed(2);
             }
-            if (Number(this.hsaAnnualVal) > this.hsaMax ) {
+            if (Number(this.hsaAnnualVal) > this.hsaMax) {
                 this.hsaAnnualVal = this.hsaMax;
             }
             this.hsaAnnualRem = this.hsaAnnualVal - this.hsaAnnualDone;
         },
-       
-        hsaOpt: function() {
-            
+
+        hsaOpt: function () {
+
+        }
+    },
+    methods: {
+        scenarioSwitch: function () {
+            var self = this;
+            var currentVal = this.hsaAnnualVal;
+            if (this.hsaAnnualDone == null) {
+                this.hsaAnnualDone = 500;
+                this.perPayCount = 12;
+                
+                setTimeout(function() {
+                    self.hsaAnnualVal = null;
+                    setTimeout(function() {
+                        self.hsaAnnualVal = currentVal;
+                    },1)
+                },1);
+
+            } else {
+                this.hsaAnnualDone = null;
+                this.perPayCount = 26;
+                setTimeout(function() {
+                    self.hsaAnnualVal = null;
+                    setTimeout(function() {
+                        self.hsaAnnualVal = currentVal;
+                    },1)
+                },1);
+            }
         }
     }
 })
