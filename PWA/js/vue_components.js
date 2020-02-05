f2ace18c327ebdd4836c5759ac4a974c0414f99d
nhugh406 <nhughes@basusa.com>
2020-02-05 17:26:34 -0500

    testing weather API

---
+++ PWA/js/vue_components.js
@@ -0,0 +1,58 @@
+var mainBlock = new Vue({
+    el: '#app',
+    data: {
+        weatherArrayLoc: [],
+        weatherArrayDat: [],
+        weatherArrayFore: [],
+        dataURI: 'https://api.weatherapi.com/v1/current.json?key=149cd2b7169e49ff9d3171456200502&q=',
+        dataURIFore: 'https://api.weatherapi.com/v1/forecast.json?key=149cd2b7169e49ff9d3171456200502&days=5&q=',
+        queryString: 19525,
+        fullURI: null,
+        weatherLoaded: false
+    },
+    methods: {
+        getWeather: function () {
+            var self = this;
+            $.ajax({
+                type: "GET",
+                url: this.dataURI + this.queryString,
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                success: function (data) {
+                    
+                    self.weatherArrayLoc.push(data);
+
+                },
+                error: function(xhr) {
+                    alert(xhr.responseText);
+                  }
+            }).then(function() {
+                self.weatherLoaded = true;
+            });
+
+
+            $.ajax({
+                type: "GET",
+                url: this.dataURIFore + this.queryString,
+                data: JSON.stringify({}),
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                success: function (data) {
+                    var tempFore = data.forecast.forecastday;
+                    self.weatherArrayFore = data.forecast.forecastday;
+                },
+                error: function (xhr) {
+                    alert(xhr.responseText);
+                }
+            });
+        }
+    },
+    watch: {
+        queryString: function() {
+            this.fullURI = this.dataURI + this.queryString;
+        }
+    },
+    created: function() {
+        this.getWeather();
+    }
+})
