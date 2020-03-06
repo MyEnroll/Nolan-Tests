ba48f1b88cfab373cd3b7d319d1871a733e4ebea
nhugh406 <nhughes@basusa.com>
2020-03-06 13:44:34 -0500

    upating

--- HSA/js/main.js
+++ HSA/js/main.js
@@ -5,28 +5,41 @@ var HSAInput = new Vue({
         hsaBWK: null,
         hsaOpt: null,
         hsaMax: 7100,
+        hsaMaxMod: 7100,
         perPayCount: 26,
         chosenHSAPlan: 'HSA Plan 1',
         hsaAnnualDone: null,
-        hsaAnnualRem: null
+        hsaAnnualRem: null,
+        additional55: false,
 
 
     },
     watch: {
         hsaAnnualVal: function () {
-            if ((Number(this.hsaAnnualVal) + this.hsaAnnualDone) <= this.hsaMax) {
+            if ((Number(this.hsaAnnualVal) + this.hsaAnnualDone) <= this.hsaMaxMod ) {
                 this.hsaBWK = ((Number(this.hsaAnnualVal) - Number(this.hsaAnnualDone)) / this.perPayCount).toFixed(2);
             } else {
                 this.hsaBWK = ((Number(this.hsaAnnualVal) - Number(this.hsaAnnualDone)) / this.perPayCount).toFixed(2);
             }
-            if (Number(this.hsaAnnualVal) > this.hsaMax) {
-                this.hsaAnnualVal = this.hsaMax;
+            if (Number(this.hsaAnnualVal) > this.hsaMaxMod ) {
+                this.hsaAnnualVal = this.hsaMaxMod ;
+            }
+            if ((Number(this.hsaAnnualVal) + this.hsaAnnualDone) > this.hsaMaxMod ) {
+                this.hsaAnnualVal = this.hsaAnnualRem;
             }
             this.hsaAnnualRem = this.hsaAnnualVal - this.hsaAnnualDone;
         },
 
-        hsaOpt: function () {
-
+        additional55: function () {
+            var self = this;
+            if (self.additional55) {
+                self.hsaMaxMod = self.hsaMax + 1000;
+            } else {
+                self.hsaMaxMod = self.hsaMax;
+                if (self.hsaAnnualVal > self.hsaMaxMod) {
+                    self.hsaAnnualVal = self.hsaMaxMod;
+                }
+            }
         }
     },
     methods: {
