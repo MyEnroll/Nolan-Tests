243fbe3939f4adedf646c131389d095a7ca5763d
nhugh406 <nhughes@basusa.com>
2020-03-09 17:12:10 -0400

    TESTIN

--- keypress_test/js/vue_components.js
+++ keypress_test/js/vue_components.js
@@ -2,19 +2,28 @@ var scanner = new Vue({
     el: '#scannertest',
     data: {
         scannedItems: [],
-        scannedRaw: ''
+        scannedRaw: '',
+        editPrep: null
     },
     methods: {
         pushtoScanned: function () {
             var self = this;
-            self.scannedItems.push(self.scannedRaw);
-            self.scannedRaw = '';
-            $('#rawInput').focus();
+            if (self.editPrep == null) {
+                var self = this;
+                self.scannedItems.push(self.scannedRaw);
+                self.scannedRaw = '';
+                $('#rawInput').focus();
+            } else {
+                Vue.set(scanner.scannedItems, scanner.editPrep, self.scannedRaw);
+                self.scannedRaw = '';
+                $('#rawInput').focus();
+            }
+
         },
         percentScan: function (event) {
             var self = this;
-            if (event.keyCode == 16) {
-                var scannedClean = self.scannedRaw.substring(0,self.scannedRaw.length -1);
+            if (event.keyCode == 16 && self.editPrep == null) {
+                var scannedClean = self.scannedRaw.substring(0, self.scannedRaw.length - 1);
                 self.scannedItems.push(scannedClean);
                 self.scannedRaw = '';
                 $('#rawInput').focus();
@@ -24,6 +33,13 @@ var scanner = new Vue({
         removeScanned: function (data) {
             var self = this;
             self.scannedItems.splice(data, 1)
+        },
+        editInline: function (data) {
+            var self = this;
+            self.editPrep = data;
+            console.log(data);
+            $('#rawInput').focus();
+            self.scannedRaw = '';
         }
     }
 })
