86c91d637d9b3b526c679505a9add2447338119d
nhugh406 <nhughes@basusa.com>
2020-04-24 21:57:29 -0400

    syncing

--- login/js/login_primary.js
+++ login/js/login_primary.js
@@ -2,13 +2,19 @@ var vm = new Vue({
 	el: '#loginBlock',
 	data: {
 		bg_image: '',
+		dailyImage: null,
 		imageArray: [],
 	},
 	methods: {
 		getDailyWallpaper: function () {
 			var self = this;
-			$.getJSON('https://picsum.photos/v2/list?page=2&limit=1', function (e) {
-				self.imageArray = e;
+
+			$.getJSON('data/wallpapers.json', function (e) {
+				self.imageArray = e.value;
+			}).done(function (e) {
+				self.dailyImage =
+					self.imageArray[Math.floor(Math.random() * self.imageArray.length)];
+				self.bg_image = self.dailyImage.contentUrl;
 			});
 		},
 	},
