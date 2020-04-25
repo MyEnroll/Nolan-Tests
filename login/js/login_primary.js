16c22244a657bd500b1b922d8addf1d7494ec783
nhugh406 <nhughes@basusa.com>
2020-04-24 23:59:16 -0400

    adding rotating images

--- login/js/login_primary.js
+++ login/js/login_primary.js
@@ -4,22 +4,43 @@ var vm = new Vue({
 		bg_image: '',
 		dailyImage: null,
 		imageArray: [],
+		infoCards: [],
+		selectedPeopleImage: '',
+		peopleImages: [
+			'pictures/1.png',
+			'pictures/2.png',
+			'pictures/3.png',
+			'pictures/4.png',
+			'pictures/5.png',
+			'pictures/6.png',
+			'pictures/7.png',
+		],
 	},
 	methods: {
-		getDailyWallpaper: function () {
+		getImages: function () {
 			var self = this;
-
 			$.getJSON('data/wallpapers.json', function (e) {
 				self.imageArray = e.value;
 			}).done(function (e) {
 				self.dailyImage =
 					self.imageArray[Math.floor(Math.random() * self.imageArray.length)];
 				self.bg_image = self.dailyImage.contentUrl;
+				self.selectedPeopleImage =
+					self.peopleImages[
+						Math.floor(Math.random() * self.peopleImages.length)
+					];
+			});
+		},
+		getInfoCards: function () {
+			var self = this;
+			$.getJSON('data/infocards.json', function (e) {
+				self.infoCards = e;
 			});
 		},
 	},
 	created: function () {
 		var self = this;
-		self.getDailyWallpaper();
+		self.getImages();
+		self.getInfoCards();
 	},
 });
