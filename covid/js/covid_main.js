ee8e0f247c07ead46c998e3f762f03aa6a23d053
nhugh406 <nhughes@basusa.com>
2020-05-02 23:50:30 -0400

    pushing covid tool

--- covid/js/covid_main.js
+++ covid/js/covid_main.js
@@ -1,5 +1,9 @@
 var vm = new Vue({
 	el: '#covidMain',
+
+	components: {
+		apexchart: VueApexCharts,
+	},
 	data: {
 		dailyData: [],
 		stateList: [
@@ -59,7 +63,58 @@ var vm = new Vue({
 			'WV',
 			'WY',
 		],
-		selState: '',
+		selState: 'PA',
+		confirmed: [],
+		death: [],
+		dates: [],
+		series: [
+			{
+				name: 'Confirmed Cases',
+				data: [],
+			},
+			{
+				name: 'Deaths',
+				data: [],
+			},
+		],
+		chartOptions: {
+			chart: {
+				type: 'bar',
+				height: 350,
+				stacked: true,
+				toolbar: {
+					show: true,
+				},
+				zoom: {
+					enabled: true,
+				},
+			},
+			responsive: [
+				{
+					breakpoint: 480,
+					options: {
+						legend: {
+							show: false,
+						},
+					},
+				},
+			],
+			plotOptions: {
+				bar: {
+					horizontal: false,
+				},
+			},
+			xaxis: {
+				type: 'datetime',
+			},
+			legend: {
+				position: 'right',
+				offsetY: 40,
+			},
+			fill: {
+				opacity: 1,
+			},
+		},
 	},
 	methods: {
 		getDaily: function () {
@@ -67,9 +122,7 @@ var vm = new Vue({
 			$.getJSON('https://covidtracking.com/api/v1/states/daily.json', function (
 				e,
 			) {
-				self.dailyData = e.filter(function (n) {
-					return (n.state = self.selState);
-				});
+				self.dailyData = e;
 			});
 		},
 		getStates: function () {
@@ -80,8 +133,40 @@ var vm = new Vue({
 				console.log(Z);
 			});
 		},
+		numberWithCommas: function (x) {
+			return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+		},
 	},
+	watch: {
+		dailyData: function () {
+			var self = this;
+			$.each(self.dailyDataState, function (key, value) {
+				self.confirmed.push([value.dateChecked, value.positive]);
+			});
+			$.each(self.dailyDataState, function (key, value) {
+				self.death.push([value.dateChecked, value.death]);
+			});
 
+			self.series = [
+				{
+					name: 'Confirmed Cases',
+					data: self.confirmed.slice(0, 5),
+				},
+				{
+					name: 'Deaths',
+					data: self.death.slice(0, 5),
+				},
+			];
+		},
+	},
+	computed: {
+		dailyDataState: function () {
+			var self = this;
+			return self.dailyData.filter(function (n) {
+				return n.state == self.selState;
+			});
+		},
+	},
 	created: function () {
 		var self = this;
 		self.getDaily();
